e1789220e37c8f8b40aabfb223f1e5650e960136
Jon Schneider <jschneider2013@gmail.com>
2018-08-11 00:17:12 -0400

    basic player + wall

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+export (float) var SPEED = 100
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+	var input_dir  = Vector2(0, 0)
+	if Input.is_action_pressed("ui_right"):
+		input_dir += Vector2(1, 0)
+	elif Input.is_action_pressed("ui_left"):
+		input_dir += Vector2(-1, 0)
+		
+	
+	if Input.is_action_pressed("ui_up"):
+		input_dir += Vector2(0, -1)
+	elif Input.is_action_pressed("ui_down"):
+		input_dir += Vector2(0, 1)
+	
+	
+	move_and_slide(SPEED*input_dir)
+	
