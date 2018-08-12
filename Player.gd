32621b007acd04c47b0baebc1c02df0f830b7fbb
Jon Schneider <jschneider2013@gmail.com>
2018-08-12 19:35:49 -0400

    player autoshoots and gets stunned when hit

--- Player.gd
+++ Player.gd
@@ -3,7 +3,8 @@ extends KinematicBody2D
 export (float) var SPEED = 100
 export (PackedScene) var Bullet = preload("Bullet.tscn")
 
-const MAX_BULLETS = 5
+enum {PLAYER_ACTIVE, PLAYER_STUNNED}
+var player_state = PLAYER_ACTIVE
 
 signal picked_up_item(item)
 
@@ -13,32 +14,28 @@ func _ready():
 	pass
 
 func _process(delta):
-	var input_dir  = Vector2(0, 0)
-	if Input.is_action_pressed("ui_right"):
-		input_dir += Vector2(1, 0)
-	elif Input.is_action_pressed("ui_left"):
-		input_dir += Vector2(-1, 0)
+	if player_state == PLAYER_ACTIVE:
+		var input_dir  = Vector2(0, 0)
+		if Input.is_action_pressed("ui_right"):
+			input_dir += Vector2(1, 0)
+		elif Input.is_action_pressed("ui_left"):
+			input_dir += Vector2(-1, 0)
+			
+		if Input.is_action_pressed("ui_up"):
+			input_dir += Vector2(0, -1)
+		elif Input.is_action_pressed("ui_down"):
+			input_dir += Vector2(0, 1)
 		
-	
-	if Input.is_action_pressed("ui_up"):
-		input_dir += Vector2(0, -1)
-	elif Input.is_action_pressed("ui_down"):
-		input_dir += Vector2(0, 1)
-	
-	if Input.is_action_just_pressed("shoot"):
-		if $bullets.get_child_count() < MAX_BULLETS:
-			var bullet = Bullet.instance()
-			bullet.position = position
-			bullet.damage = total_attack()
-			$bullets.add_child(bullet)
-	
-	move_and_slide(SPEED*(input_dir.normalized()))
+		move_and_slide(SPEED*(input_dir.normalized()))
 	
 func try_pick_up(game_item):
 	emit_signal("picked_up_item", game_item)
 
 func take_damage(damage):
-	print("oh no, player hit")
+	# stun player
+	player_state = PLAYER_STUNNED
+	$StunTimer.start()
+	
 	
 func total_attack():
 	return stats().base_attack_power + stats().bonus_attack
@@ -48,3 +45,14 @@ func doomify():
 	
 func stats():
 	return get_node("Stats")
+
+func _on_BulletTimer_timeout():
+	if player_state == PLAYER_ACTIVE:
+		var bullet = Bullet.instance()
+		bullet.position = position
+		bullet.damage = total_attack()
+		$bullets.add_child(bullet)
+
+func _on_StunTimer_timeout():
+	# stun over
+	player_state = PLAYER_ACTIVE
