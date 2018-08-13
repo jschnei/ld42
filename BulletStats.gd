29facdfaabe7a31db964c7fceeccf88ebde6594f
Jon Schneider <jschneider2013@gmail.com>
2018-08-12 23:28:34 -0400

    whoops actually include Enemy files

---
+++ BulletStats.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var base_damage = 0
+var red_damage = 0
+var green_damage = 0
+var blue_damage = 0
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
